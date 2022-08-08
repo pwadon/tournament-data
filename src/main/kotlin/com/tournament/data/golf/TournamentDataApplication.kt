1ec90f34b7c02ad99fa588cff40660bfd7554599
Paladyn <p.wadon@gazeta.pl>
2022-08-08 21:04:04 +0200

    project structure and spring initializer

---
+++ src/main/kotlin/com/tournament/data/golf/TournamentDataApplication.kt
@@ -0,0 +1,11 @@
+package com.tournament.data.golf
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TournamentDataApplication
+
+fun main(args: Array<String>) {
+    runApplication<TournamentDataApplication>(*args)
+}
---
+++ src/test/kotlin/com/tournament/data/golf/TournamentDataApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.tournament.data.golf
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TournamentDataApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
